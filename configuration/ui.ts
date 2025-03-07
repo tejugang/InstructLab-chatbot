7e06e85c93c5f0dad6c0b91874703c8ec4f77a16
Teju Gangisetty <111475372+tejugang@users.noreply.github.com>
2025-03-07 02:11:58 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const CHAT_HEADER: string = "Welcome to the Chat with Teju's AI Assistant!";
+export const MESSAGE_PLACEHOLDER: string = "Type your message here...";
+export const FOOTER_MESSAGE: string = "Customized AI by Teju Gangisetty at UNC";
+export const CLEAR_BUTTON_TEXT: string = "Clear Chat";
+export const PAGE_TITLE: string = "Teju's AI Assistant";
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
