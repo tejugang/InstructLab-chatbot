2af7207604c3e0320689f04f4a107c55cca45dbe
Teju Gangisetty <111475372+tejugang@users.noreply.github.com>
2025-03-07 02:32:30 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -5,6 +5,6 @@ export const MESSAGE_PLACEHOLDER: string = "Type your message here...";
 export const FOOTER_MESSAGE: string = "Customized AI by Teju Gangisetty at UNC";
 export const CLEAR_BUTTON_TEXT: string = "Clear Chat";
 export const PAGE_TITLE: string = "Teju's AI Assistant";
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const PAGE_DESCRIPTION: string = `Chat with ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
